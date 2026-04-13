151a341312049c52830de124f24052cee4843eba
basemosama <engbasemosama@gmail.com>
2026-04-13 07:53:47 +0200

    fix: clustering

--- packages/google_maps_flutter/google_maps_flutter_android/lib/src/messages.g.dart
+++ packages/google_maps_flutter/google_maps_flutter_android/lib/src/messages.g.dart
@@ -4459,23 +4459,13 @@ class MapsInspectorApi {
       <Object?>[clusterManagerId],
     );
     final pigeonVar_replyList = await pigeonVar_sendFuture as List<Object?>?;
-    if (pigeonVar_replyList == null) {
-      throw _createConnectionError(pigeonVar_channelName);
-    } else if (pigeonVar_replyList.length > 1) {
-      throw PlatformException(
-        code: pigeonVar_replyList[0]! as String,
-        message: pigeonVar_replyList[1] as String?,
-        details: pigeonVar_replyList[2],
-      );
-    } else if (pigeonVar_replyList[0] == null) {
-      throw PlatformException(
-        code: 'null-error',
-        message: 'Host platform returned null value for non-null return value.',
-      );
-    } else {
-      return (pigeonVar_replyList[0] as List<Object?>?)!
-          .cast<PlatformCluster>();
-    }
+
+    final Object? pigeonVar_replyValue = _extractReplyValueOrThrow(
+      pigeonVar_replyList,
+      pigeonVar_channelName,
+      isNullValid: false,
+    );
+    return (pigeonVar_replyValue! as List<Object?>).cast<PlatformCluster>();
   }
 
   Future<PlatformCameraPosition> getCameraPosition() async {
